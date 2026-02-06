412cbc423af67809c09b061300f969c98a8a900d
Luiz <Luizog235@gmail.com>
2026-02-06 16:24:27 -0300

    update imports

--- app/src/main/java/com/processdumper/MainActivity.kt
+++ app/src/main/java/com/processdumper/MainActivity.kt
@@ -21,6 +21,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
