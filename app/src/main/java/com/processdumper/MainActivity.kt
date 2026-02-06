f94728839b80e9477c8f3213b17b1f3fb259c884
Luiz <Luizog235@gmail.com>
2026-02-06 15:21:31 -0300

    Update to show process on modal

--- app/src/main/java/com/processdumper/MainActivity.kt
+++ app/src/main/java/com/processdumper/MainActivity.kt
@@ -8,11 +8,18 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -25,6 +32,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
 import com.processdumper.model.ProcessInfo
@@ -51,35 +60,71 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     var itens by remember {
         mutableStateOf(listOf("Item 1", "Item 2", "Item 3"))
     }
+    var showModal by remember { mutableStateOf(false) }
+    var selectedItem by remember { mutableStateOf<String?>(null) }
+
+
     var lastUpdated by remember { mutableStateOf(System.currentTimeMillis()) }
     val context = LocalContext.current
     if(!context.checkStoragePermission()) {
         Toast.makeText(context, "PERMISSÃO DE ARMAZENAMENTO NÃO CONCEDIDA", Toast.LENGTH_SHORT).show()
     }
+    if (showModal) {
+        Dialog(onDismissRequest = { showModal = false }) {
+
+            Box(
+                modifier = Modifier
+                    .size(280.dp) // quadrado
+                    .background(
+                        color = MaterialTheme.colorScheme.surface,
+                        shape = RoundedCornerShape(12.dp)
+                    )
+                    .padding(16.dp)
+            ) {
+                LazyColumn {
+                    items(itens) { item ->
+                        Text(
+                            text = item,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(12.dp)
+                                .clickable {
+                                    selectedItem = item
+                                    showModal = false
+                                }
+                        )
+                    }
+                }
+
+            }
+        }
+    }
+
+
+
 
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
 
-        Text(text= "Processos: ")
-        Column {
-            itens.forEach { item -> Text(item) }
-
-        }
         Text(text="Atualizado: $lastUpdated")
-
         Button(
             onClick = { getListProcess( newList = { list ->
                 itens = list
             }, context = context); lastUpdated = System.currentTimeMillis()}
         ){
             Text("Recarregar processos")
+        }
+
+        Button(onClick = { showModal = true }) {
+            Text("Selecionar processo")
         }
 
     }
@@ -122,7 +167,7 @@ fun getListProcess(newList: (List<String>) -> Unit, context: Context){
         var pid : Int? = processInfos.get(1).toIntOrNull();
         processManager.add(ProcessInfo(context, packageName, pid))
     }
-    
+
     newList(processManager.getAllFormated());
 }
 
