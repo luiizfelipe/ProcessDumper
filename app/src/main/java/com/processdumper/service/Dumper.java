5063592ce9915cba862f6cf1a493460064b3e22b
Luiz <Luizog235@gmail.com>
2026-02-13 17:56:33 -0300

    criado funções principais de um dumper

--- app/src/main/java/com/processdumper/service/Dumper.java
+++ app/src/main/java/com/processdumper/service/Dumper.java
@@ -1,8 +1,41 @@
 package com.processdumper.service;
 
+import com.processdumper.model.ProcessInfo;
+import com.processdumper.utils.LogManager;
+import com.topjohnwu.superuser.Shell;
+import com.topjohnwu.superuser.io.SuFile;
+
 public class Dumper {
 
+    ProcessInfo process;
+
+    public Dumper(ProcessInfo process){
+        this.process = process;
+    }
+
     public void init(){
+        createDirectory("output/");
 
     }
+
+
+
+    private void dumpFile(){
+
+    }
+
+    private boolean dumpMemory(){
+
+
+
+        return true;
+    }
+
+    private boolean createDirectory(String path){
+        LogManager.log("Diretório criado:" + path );
+        SuFile directory = new SuFile(path);
+
+        return directory.mkdirs();
+    }
+
 }
