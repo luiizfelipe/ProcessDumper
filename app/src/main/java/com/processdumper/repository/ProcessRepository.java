e4d77a483039fb9fb80953dbf12667565e794367
Luiz <Luizog235@gmail.com>
2026-02-13 17:57:19 -0300

    modificando processrepository de kotlin para java

---
+++ app/src/main/java/com/processdumper/repository/ProcessRepository.java
@@ -0,0 +1,48 @@
+package com.processdumper.repository;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.processdumper.model.ProcessInfo;
+import com.processdumper.utils.LogManager;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ProcessRepository {
+    private final HashMap<String,ProcessInfo> processes = new HashMap<>();
+
+    public HashMap<String, ProcessInfo> getProcesses() {
+        return processes;
+    }
+
+    public ProcessInfo getProcessInfo(String packageName){
+        return processes.get(packageName);
+    }
+
+    public List<String> getAllFormated(){
+        List<String> processFormatted = new ArrayList<>();
+
+        for (ProcessInfo process : this.processes.values()) {
+            processFormatted.add(process.packageName);
+        }
+
+        return processFormatted;
+    }
+
+    public boolean add(Context context, String packageName, Integer pid){
+        if(pid <= 0){
+            return false;
+        }
+        ProcessInfo info = processes.get(packageName);
+        if(info != null){
+            info.pids.add(pid);
+        }else {
+            processes.put(packageName,
+                    new ProcessInfo(context, packageName, new ArrayList<>(List.of(pid))));
+            ;
+        }
+        return true;
+    }
+}
