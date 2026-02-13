c82f60a50285f4f999838440c73c6fb6ab52952d
Luiz <Luizog235@gmail.com>
2026-02-13 17:56:58 -0300

    modificado processinfo de kotlin para java

---
+++ app/src/main/java/com/processdumper/model/ProcessInfo.java
@@ -0,0 +1,31 @@
+package com.processdumper.model;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+
+import com.processdumper.utils.LogManager;
+
+import java.util.List;
+
+public class ProcessInfo {
+    public String appName;
+    public String packageName;
+    public List<Integer> pids;
+
+    public ProcessInfo(Context context, String packageName, List<Integer> pids){
+        ApplicationInfo applicationInfo = null;
+        try {
+            applicationInfo = context.getPackageManager().getApplicationInfo(packageName,0);
+
+        } catch (Exception e) {
+            LogManager.log("Not found application info");
+        }
+        finally {
+            this.packageName = packageName;
+            this.pids = pids;
+
+            this.appName = (applicationInfo != null ? context.getPackageManager().getApplicationLabel(applicationInfo).toString() : "App Name not found");
+        }
+    }
+
+}
