465a507b53ff3be07206f88726f5fef7176d464e
Luiz <Luizog235@gmail.com>
2026-02-16 16:50:28 -0300

    criando função de pegar nome dos arquivos e fazendo o serviço seguir um fluxo de acordo com o DumpAction

---
+++ app/src/main/java/com/processdumper/model/enums/dumper/DumpAction.java
@@ -0,0 +1,8 @@
+package com.processdumper.model.enums.dumper;
+
+public enum DumpAction  {
+    PROCEED,
+    ASK_USER_FILE,
+    NOT_FOUND_FILE,
+    ERROR
+}
--- app/src/main/java/com/processdumper/service/Dumper.java
+++ app/src/main/java/com/processdumper/service/Dumper.java
@@ -1,40 +1,66 @@
 package com.processdumper.service;
 
+import android.util.Log;
+
+import com.processdumper.model.enums.dumper.DumpAction;
+import com.processdumper.model.mapinfo.MapInfo;
 import com.processdumper.model.ProcessInfo;
 import com.processdumper.utils.LogManager;
 import com.topjohnwu.superuser.Shell;
 import com.topjohnwu.superuser.io.SuFile;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Dumper {
 
+    MemoryManagement memoryManagement;
     ProcessInfo process;
+    String fileName = "";
+
 
     public Dumper(ProcessInfo process){
         this.process = process;
+        this.memoryManagement = new MemoryManagement();
     }
 
-    public void init(){
-        createDirectory("output/");
-
+    public DumpAction init(){
+        List<String> filesName = this.getFileNames();
+        int amountFiles = filesName.size();
+        if(amountFiles == 0){
+            return DumpAction.NOT_FOUND_FILE;
+        }
+        if(amountFiles == 1){
+            return DumpAction.PROCEED;
+        }
+        return DumpAction.ASK_USER_FILE;
     }
 
-
+    private List<String> getFileNames(){
+        List<MapInfo> maps = this.memoryManagement.getAllMaps(process, fileName);
+        List<String> filesName = new ArrayList<>();
+        for (MapInfo map : maps) {
+            String fileName = map.getFileName();
+            if(fileName.isEmpty()) continue;
+            filesName.add(fileName);
+            LogManager.log(fileName);
+        }
+        return filesName;
+    }
 
     private void dumpFile(){
 
     }
 
-    private boolean dumpMemory(){
-
+    private boolean dumpMemory() {
 
 
         return true;
     }
 
     private boolean createDirectory(String path){
-        LogManager.log("Diretório criado:" + path );
         SuFile directory = new SuFile(path);
-
         return directory.mkdirs();
     }
 
