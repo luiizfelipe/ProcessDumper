87091b7c8ae47e110ce8e2210417fd325e09cd51
Luiz <Luizog235@gmail.com>
2026-02-16 21:46:21 -0300

    criando handle simples para as funções de click

--- app/src/main/java/com/processdumper/screens/HomeScreen.kt
+++ app/src/main/java/com/processdumper/screens/HomeScreen.kt
@@ -12,29 +12,63 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import com.processdumper.components.ListModal
 import com.processdumper.components.LogScreen
-import com.processdumper.components.ProcessListModal
 import com.processdumper.getListProcess
 import com.processdumper.model.ProcessInfo
+import com.processdumper.model.enums.dumper.DumpAction
 import com.processdumper.repository.ProcessRepository
 import com.processdumper.service.Dumper
-import com.processdumper.utils.LogManager
 
 @Composable
 fun HomeScreen(name: String, modifier: Modifier = Modifier) {
-    var itens by remember {
-        mutableStateOf(listOf("a"))
+    var items by remember {
+        mutableStateOf(listOf(""))
     }
     var showModal by remember { mutableStateOf(false) }
-    var selectedItem by remember { mutableStateOf<ProcessInfo?>(null) }
+    var selectedProcess by remember { mutableStateOf<ProcessInfo?>(null) }
+    var selectedFileName by remember { mutableStateOf<String?>(null) }
     var processRepository by remember { mutableStateOf(ProcessRepository()) }
     var lastUpdated by remember { mutableStateOf(System.currentTimeMillis()) }
     val context = LocalContext.current
 
-    ProcessListModal(showModal, onDismiss = { showModal = false }, clickableLine = { line ->
-        selectedItem = processRepository.getProcessInfo(line);
+
+    val onClickSelected : () -> Unit = {
+        getListProcess(
+            newList = { _processRepository ->
+                if (_processRepository != null) {
+                    processRepository = _processRepository;
+                    items = processRepository.getAllFormated()
+                }
+            }, context = context
+        );
+        lastUpdated = System.currentTimeMillis();
+        showModal = true;
+    }
+
+    val onClickDump : () -> Unit = {
+        var dumper = Dumper(selectedProcess)
+        var action: DumpAction = dumper.init();
+        if(action == DumpAction.ASK_USER_FILE){
+            showModal = true;
+            items = dumper.getFilesName();
+        }
+        if (action == DumpAction.NOT_FOUND_FILE){
+
+        }
+        if(action == DumpAction.PROCEED){
+
+        }
+        if(action == DumpAction.ERROR){
+
+        }
+    }
+
+    
+    ListModal(showModal, onDismiss = { showModal = false }, clickableLine = { line ->
+        selectedProcess = processRepository.getProcessInfo(line);
         showModal = false;
-    }, itens);
+    }, items);
 
 
     Column(
@@ -42,33 +76,21 @@ fun HomeScreen(name: String, modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.Start
     ) {
-        Button(onClick = {
-            getListProcess(
-                newList = { _processRepository ->
-                    if (_processRepository != null) {
-                        processRepository = _processRepository;
-                        itens = processRepository.getAllFormated()
-                    }
-                }, context = context
-            );
-            lastUpdated = System.currentTimeMillis();
-            showModal = true;
-
-        }) {
+        Button(onClick = onClickSelected) {
             Text("Selecionar processo")
         }
-        Button(onClick = {
-            var dumper = Dumper(selectedItem)
-            dumper.init();
-        }) {
 
-            Text("Dump")
+        if (selectedProcess != null) {
+            Button(onClick = onClickDump) {
+                Text("Dump")
+            }
         }
+
         LogScreen()
 
         Text(text = "Atualizado: $lastUpdated")
-        if (selectedItem != null) {
-            Text(text = "Processo selecionado: ${selectedItem?.packageName}")
+        if (selectedProcess != null) {
+            Text(text = "Processo selecionado: ${selectedProcess?.packageName}")
         }
     }
 
